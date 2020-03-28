63b12125dcbdddc3574b102c9e4875368f3bb0b0
Karamullah Agha <saad@Karamullahs-MacBook-Pro.local>
2020-03-27 19:47:23 -0600

    updated post loading, added upvote and downvotes

---
+++ upvote.php
@@ -0,0 +1,34 @@
+<?php
+
+
+$postID = $_GET['postid'];
+
+if(isset($_COOKIE["username"])) {
+            $username = "mohh3660";
+            $password = "glxcdj";
+
+            $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+            
+            $sql = "UPDATE POST SET upvotes= upvotes +  1 WHERE id = $postID";
+
+
+            if($conn->query($sql))
+            {
+                
+                header("Location: posts.php");
+            }
+        } else {
+            $err = $conn->errno; 
+            $errtxt = $conn->error; 
+            if($err)
+            {
+                echo "<h3>Unable to upvote </h3>";
+                echo "Error code: $err.\n$errtxt\nPlease contact an Admin."; 
+            }
+
+
+        }
+
+
+
+?>
